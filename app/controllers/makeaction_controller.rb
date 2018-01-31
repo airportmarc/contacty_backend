534bc493b0caa34945a4d77f80622e0280a28c31
airportmarc <marcwitney@live.com>
2018-01-31 14:50:55 -0500

    Oppss

--- app/controllers/makeaction_controller.rb
+++ app/controllers/makeaction_controller.rb
@@ -5,8 +5,7 @@ class MakeactionController < ApplicationController
 
   def act
     puts params
-    account_sid = 'ACa54ac17fc5643b3d965309d443c4cbd6'
-    auth_token = 'fa232ffeb71619f2f15fd16126b5cb67'
+
 
 # set up a client to talk to the Twilio REST API
 #     @client = Twilio::REST::Client.new account_sid, auth_token
